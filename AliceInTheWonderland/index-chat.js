ced60320405c9f38c7c8045dbfead064f1a83afb
RonaldGeneral <gogo23993@gmail.com>
2022-01-03 16:41:05 +0800

    Update index-chat.js

--- AliceInTheWonderland/index-chat.js
+++ AliceInTheWonderland/index-chat.js
@@ -14,3 +14,25 @@ function showBubble(id, width, height){
 }
 
 showBubble('.XB', 100, 40);
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+    
+let testTxt = document.getElementById("test");
+
+let randomChar = ['a','b','c','d','e','f','g',1,2,3,4,5,6];
+
+let counter = 0;
+let i = setInterval(function(){
+    testTxt.innerHTML = "";
+    for(let i = 0;i < 10;i++){
+        testTxt.innerHTML += randomChar[getRandomInt(randomChar.length)];
+    }
+    counter++;
+    if(counter == 10){
+        clearInterval(i);
+        testTxt.innerHTML = "Hello world";
+    }
+
+}, 100);
